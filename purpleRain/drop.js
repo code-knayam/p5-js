996a63385f5e89e0b230039d742754a86c0bdbc9
code-knayam <mayankjain1394@gmail.com>
2017-08-02 11:09:59 +0530

    Updated speed of the rain drop

--- purpleRain/drop.js
+++ purpleRain/drop.js
@@ -2,11 +2,11 @@ function Drop() {
 
   this.x = random( width );
   this.y = random( -500, 50 );
-  this.ySpeed = random(0, 10);
+  this.ySpeed = random(3, 8);
   this.len = random(1, 10);
 
   this.fall = function() {
-    this.y = this.y + this.ySpeed;
+    this.y = this.y + this.ySpeed + 0.5;
 
     if ( this.y > height) {
       this.y = 0;
