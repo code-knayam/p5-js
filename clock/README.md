db945dbc5d6eb1da77ca862c5fd939fed5793fbd
code-knayam <mayankjain1394@gmail.com>
2017-09-22 00:37:50 +0530

    Added Fancy Clock project.

---
+++ clock/README.md
@@ -0,0 +1,12 @@
+# Purple Rain
+
+In this practice problem, we are creating a rain type effect using p5.js
+
+We are creating objects of type Drop. And each drop has its own attributes like speed, x position and y position.
+We are updating the position of the drop by the fall function, where it is updated by ySpeed - a random number.
+
+When the drop falls beyond the height of the frame, its position is again set as 0 ie on top.
+
+Watch the Rain here -
+
+https://code-knayam.github.io/p5-js/purpleRain/
---
+++ clock/sketch.js
@@ -0,0 +1,60 @@
+
+
+function setup() {
+  createCanvas(800, 800);
+  angleMode(DEGREES);
+}
+
+function draw() {
+
+  background(0);
+  translate(400,400);
+  rotate(-90);
+
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+  
+  strokeWeight(5);
+  smooth();
+  noFill();
+
+  let secondsAngle = map(sc, 0, 60, 0, 360);
+  let minuteAngle = map(mn, 0, 60, 0, 360);
+  let hourAngle = map(hr % 12 , 0, 12, 0, 360);  
+
+  //second
+  stroke(100, 150, 200);
+  arc(0, 0, 300, 300, 0, secondsAngle);
+  push();
+  rotate(secondsAngle);
+  line(0, 0, 100, 0);
+  pop();
+
+  //minutes
+  stroke(200, 100, 150);  
+  arc(0, 0, 280, 280, 0, minuteAngle);
+  push();
+  rotate(minuteAngle);
+  line(0, 0, 75, 0);
+  pop();
+
+  //hours
+  stroke(150, 200, 100);  
+  arc(0, 0, 260, 260, 0, hourAngle);
+  push();
+  rotate(hourAngle);
+  line(0, 0, 50, 0);
+  pop();
+
+  //center dot
+  stroke(255);
+  point(0,0);
+
+  //time
+  rotate(90);
+  noStroke();
+  fill(255);  
+  text( hr + " : " + mn + " : " + sc, 00, 200 );
+
+}
